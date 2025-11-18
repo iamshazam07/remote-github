130830e825fcc857d78ec83f5418bf88f7cd37a9
iamshazam07 <shazamkhan1997@gmail.com>
2025-11-18 20:20:58 +0530

    third

---
+++ CLASS/class.py
@@ -0,0 +1,38 @@
+# Python Classes and Objects
+# In the last tutorial, we learned about Python OOP. 
+# We know that Python also supports the concept of objects and classes.
+
+# An object is simply a collection of data (variables) and methods (functions). 
+# Similarly, a class is a blueprint for that object.
+
+# Before we learn about objects, let's first learn about classes in Python.
+
+# Python Classes
+# A class is considered a blueprint of objects.
+
+# We can think of the class as a sketch (prototype) of a house. 
+# It contains all the details about the floors, doors, windows, etc.
+
+# Based on these descriptions, we build the house; the house is the object.
+
+# Since many houses can be made from the same description, we can create many objects from a class.
+
+
+# Define Python Class
+# We use the class keyword to create a class in Python. For example,
+
+class ClassName:
+    # class definition
+    pass
+
+class Bike:
+    name = ""
+    gear = 0
+
+    #Here,
+
+#Bike - the name of the class
+#name/gear - variables inside the class with default values "" and 0 respectively
+
+
+#Note: The variables inside a class are called attributes.
--- forloop.py
+++
@@ -1,6 +0,0 @@
-#program to print numbers from 1-10 using for loop
-
-n=10
-
-for i in range (0,n):
-    print(n)
